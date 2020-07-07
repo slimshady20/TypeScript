7334ae34300964301a6379fe3ea3f5bec1625372
양정욱 <61002791+slimshady20@users.noreply.github.com>
2020-07-07 16:56:42 +0900

    07/07/2020
    TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import MyChatBot from "./components/MyChatbot";
+import Home from "./components/Home";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any, any>{
+    public render() {
+        return <Router>
+        <Link to="/">홈으로 이동</Link> <br/>
+        <Link to="/chat">챗봇 이동</Link>
+        <Route exact path='/chat' component = {MyChatBot}/>
+        <Route exact path='/' component = {Home}/>
+    </Router>
+    }
 }
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Home extends React.Component<any, any>{
+    public render() {
+        return <h1>홈</h1>;
+    }
+}
+
+export default Home  // export 하면 모듈 모듈을해야 다른스크립트에서 쓸 수 있다.
